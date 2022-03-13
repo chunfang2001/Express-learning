2cacea4010ddca294c917e33ee0976e379696fd3
chunfang2001 <chunfang2001@gmail.com>
2022-03-14 00:28:35 +0800

    prisma create data

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+
+const app = express()
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+    res.send("hello")
+    
+})
+
+app.get("/create",(req,res)=>{
+    const create = async () => {
+        const newUser = await prisma.User.create({
+            data:{
+                name: 'Alice',
+                email: 'alice@prisma.io',
+            }
+        })
+    }
+    create()
+    res.send("created")
+})
+
+app.get("/get",async (req,res)=>{
+    const create = async () => {
+        const newUser = await prisma.user.findMany({
+            select:{
+                name:true,
+                email:true
+            }
+        })
+        console.log(newUser)
+        
+    }
+    await create()
+    res.send("get")
+})
+
+app.listen(3000,()=>{
+    console.log("hello")
+})
